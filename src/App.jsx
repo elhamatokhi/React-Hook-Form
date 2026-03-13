7dfd505b4f5a410923d1feaa46ede38216d44048
Elhama Tokhi <bakhtmohammadtokhi@gmail.com>
2026-03-13 21:15:15 +0100

    Add loginFrom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
 import './App.css'
+import LoginForm from './components/LoginForm'
 import RegisterForm from './components/RegisterForm'
+import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showLogin, setShowLogin] = useState(true);
 
   return (
     <div>
-      <RegisterForm></RegisterForm>
+      {
+        showLogin ? (
+          <LoginForm onSwitchToRegister={()=>setShowLogin(false)}/> 
+        ) : (<RegisterForm onSwitchToLogin={() => setShowLogin(true)} />)
+      }
     </div>
   )
 }
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import { set, useForm } from "react-hook-form";
+import '../styles/login.css'
+export default function LoginForm({ onSwitchToRegister}){
+    const [success, setSuccess] = useState("")
+
+    const { 
+        register,
+        handleSubmit,
+        formState: {errors, isValid, isSubmitting},
+        reset
+    } = useForm
+    ({
+        mode:"onTouched"
+    })
+
+    function onSubmit(data){
+        console.log("User Logged In: ", data)
+        setSuccess("Login successfull!")
+    }
+
+    function handleReset(){
+        reset()
+        setSuccess("")
+    }
+
+    return (
+        <form className="login" onSubmit={handleSubmit(onSubmit)}>
+            <h2>Login</h2>
+        
+        {success && <div className="login__success">{success}</div>}
+
+        {/* Email  */}
+        <div className="login__field">
+            <label className="login__label">Email</label>
+            <input 
+            type="email" 
+            className="login__input" 
+            placeholder="you@example.com"
+            {...register("email",{required: "Email is required"})}
+            />
+
+            {errors.email && (
+                <div className="login__error">{errors.email.message}</div>
+            )}
+        </div>
+
+
+        {/* Password */}
+        <div className="logi__field">
+            <label className="login__label">Password</label>
+            <input type="password" className="login__input" 
+            placeholder="Enter password"
+            {...register("password", {required: "Password is required"})}
+            />
+
+            {errors.password && (
+                <div className="login__error">{errors.password.message}</div>
+            )}
+        </div>
+
+        {/* Actions */}
+        <div className="login__actions">
+            <button type="submit" disabled={!isValid || isSubmitting}>Login</button>
+            <button type="button" onClick={handleReset}>Reset</button>
+        </div>
+
+        <p className="login__switch">
+            Don't have an account?
+            <button type="button" onClick={onSwitchToRegister}> Register</button>
+        </p>
+
+        </form>
+    )
+
+}
+
